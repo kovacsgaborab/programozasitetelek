c247f46fce043d9ae67c657e08a7d07f05920cc0
kovacsgaborab <kovacsgaborab@gmail.com>
2020-09-11 10:16:15 +0200

    Előkészítés

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# programozasitetelek
+# Programozási Tételek
+
+## Elõkészítés
+ - 10 elemû, egész számot tartalmazó tömb létrehozása
+ - A tömb elemei: 
+      - 12, 7, 2, 4, 45, 99, 1, 8, 95, 50
+ - A tömb az összes metódusban látható legyen
+ - A tombkiiras() metódus létrehozása
+     - nem ad vissza értéket
+     - nincs paramétere
+     - a tömb elemeit vesszõvel elválasztva egy sorban a console-ra írja
--- programozasitetelek/Program.cs
+++ programozasitetelek/Program.cs
@@ -8,8 +8,20 @@ namespace programozasitetelek
 {
     class Program
     {
+        static int[] tomb = new int[] {12, 7, 2, 4, 45, 99, 1, 8, 95, 50};
         static void Main(string[] args)
         {
+            tombkiiras();
+
+            Console.ReadKey();
+        }
+
+        private static void tombkiiras()
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write("{0}, ", tomb[i]);
+            }
         }
     }
 }
